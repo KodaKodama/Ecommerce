25963d56ee911450f51074f5a23b75d685422857
kodakodama <163364396+KodaKodama@users.noreply.github.com>
2024-09-07 04:05:16 +0530

    feat: add authentication middleware for secure routes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userController = require('../controller/userController');
+const auth = require('../middlewares/auth');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/logout', userController.logout);
 router.post('/refresh_token', userController.refreshtoken);
+router.get('/getuser', auth, userController.getUser)
 
 module.exports = router;
